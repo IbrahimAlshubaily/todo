4a81fd289904fcc0498110fc5abfe60ce58fd033
Ibrahim Alshubaily <ibrahim.alshubaily.sde@gmail.com>
2022-11-10 10:33:25 +0000

    add complete and delete functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import Tasks from './Tasks';
 import Form from './Form';
 import "./style.css"
 
+const fetchTasks = (key: string): string[] => {
+  const tasks = localStorage.getItem(key);
+  if (tasks)
+    return JSON.parse(tasks);
+  return []
+}
 
 function App() {
 
-  const [submittedTasks, setSubmittedTasks] = useState<string[]>([]);
+  const [todoTasks, setTodoTasks] = useState<string[]>(() => fetchTasks("todo-tasks"));
+  const [doneTasks, setDoneTasks] = useState<string[]>(() => fetchTasks("done-tasks"));
 
-  const submitTask = (task: string) => setSubmittedTasks([...submittedTasks, task])
+
+  useEffect(() => {
+    localStorage.setItem("todo-tasks", JSON.stringify(todoTasks))
+  }, [todoTasks])
+
+
+  useEffect(() => {
+    localStorage.setItem("done-tasks", JSON.stringify(doneTasks))
+  }, [doneTasks])
+
+
+  const submitTask = (task: string) => setTodoTasks([...todoTasks, task])
+
+  const removeTask = (removeIdx: number, isDone: boolean) => {
+    if (isDone) {
+      setDoneTasks(doneTasks.filter((_: string, idx: number) => idx !== removeIdx));
+    } else {
+      setTodoTasks(todoTasks.filter((_: string, idx: number) => idx !== removeIdx));
+    }
+  }
+
+  const completeTaks = (removeIdx: number) => {
+    setDoneTasks([...doneTasks, todoTasks[removeIdx]]);
+    removeTask(removeIdx, false);
+  }
 
   return (
     <div className="App">
       <Form submitTask={submitTask}></Form>
-      <Tasks tasks={submittedTasks} />
+      <Tasks todoTasks={todoTasks} doneTasks={doneTasks} removeTask={removeTask} completeTask={completeTaks} />
     </div>
   );
 }
--- src/Tasks.tsx
+++ src/Tasks.tsx
@@ -3,13 +3,37 @@ import "./style.css";
 
 
 interface TasksProps {
-    tasks: string[]
+    todoTasks: string[]
+    doneTasks: string[]
+    removeTask: Function
+    completeTask: Function
 }
 
 export default function Tasks(props: TasksProps): JSX.Element {
 
-    const tasksDivs = props.tasks.map(t => <div className='task'><p>{t}</p></div>)
+    const task = (title: string, idx: number, className: string) => {
+
+        const completeTaksButton = <button className='task-button'
+            onClick={() => props.completeTask(idx)}>
+            ✅
+        </button>
+        return <div key={idx} className={"task " + className}>
+            <h5>{title}</h5>
+            <div className='task-buttons'>
+                <button className='task-button'
+                    onClick={() => props.removeTask(idx, className === "done-task")}>
+                    🗑️
+                </button>
+                {className === "done-task" ? null : completeTaksButton}
+            </div>
+        </div>
+    }
+
+    const todoTasksDivs = props.todoTasks.map((title: string, idx: number) => task(title, idx, "todo-task"))
+    const doneTasksDivs = props.doneTasks.map((title: string, idx: number) => task(title, idx, "done-task"))
     return <div className='task-board'>
-        {tasksDivs}
+        <div className='tasks'>{todoTasksDivs}</div>
+        <div className='tasks done-tasks'>{doneTasksDivs}</div>
     </div>
+
 }
